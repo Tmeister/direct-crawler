69c75de69d3e82608f59446ea48e0167aebe5b2b
Tmeister <noone@tmeister.net>
2022-03-08 18:20:45 -0600

    Add SEO parser and updater

--- src/Parser.php
+++ src/Parser.php
@@ -6,6 +6,7 @@ use Exception;
 use Goutte\Client;
 use League\Csv\Reader;
 use WP_CLI;
+use WPSEO_Meta;
 
 class Parser {
 	protected string $postsFile = 'posts.csv';
@@ -24,27 +25,42 @@ class Parser {
 		$posts = $this->getBlogPosts();
 
 		foreach ( $posts as $post ) {
-			// if ($count === 10) {
-			//     break;
-			// }
+//			if ( $count === 1 ) {
+//				break;
+//			}
 
 			// Parse the external URL and get the post parts
-			$rawPost = $this->parsePost( $post[0], ++ $count );
+			$rawContent = $this->parsePost( $post[0], ++ $count );
 
 			// Skip if the post is not found
-			if ( ! $rawPost ) {
+			if ( ! $rawContent['post'] ) {
+				WP_CLI::line( 'Post not found: ' . $post[0] );
 				continue;
 			}
 
 			// Create a new WP post based on the parsed post
-			$postId = $this->createPost( $rawPost );
-			WP_CLI::line( 'New post created: ' . $postId . ' - ' . $rawPost['post_title'] );
+			$postId = $this->createPost( $rawContent['post'] );
+			WP_CLI::line( 'New post created: ' . $postId . ' - ' . $rawContent['post']['post_title'] );
 
 			// Get, store and set the post's featured image
-			if ( $rawPost['featured_image'] ) {
-				FeaturedImage::store( $rawPost['featured_image'], $postId );
+			if ( $rawContent['post']['featured_image'] ) {
+				FeaturedImage::store( $rawContent['post']['featured_image'], $postId );
 			}
 
+			// Update the post's meta if any
+			try {
+				WP_CLI::line( 'Updating post meta...' );
+				WP_CLI::line( print_r( $rawContent['meta'], true ) );
+				if ( isset( $rawContent['meta'] ) && $rawContent['meta']['title'] && ! empty( $rawContent['meta']['title'] ) ) {
+					WPSEO_Meta::set_value( 'title', $rawContent['meta']['title'], $postId );
+				}
+				if ( isset( $rawContent['meta'] ) && $rawContent['meta']['description'] && ! empty( $rawContent['meta']['description'] ) ) {
+					WPSEO_Meta::set_value( 'metadesc', $rawContent['meta']['description'], $postId );
+				}
+			} catch
+			( Exception $e ) {
+				WP_CLI::line( 'Error updating post meta: ' . $e->getMessage() );
+			}
 			WP_CLI::line( '==========================================================' );
 		}
 
@@ -77,12 +93,35 @@ class Parser {
 
 			$formattedDate = date( 'Y-m-d H:i:s', strtotime( $date ) );
 
-			return [
+			$post = [
 				'post_title'     => $title,
 				'post_content'   => $content,
 				'post_date'      => $formattedDate,
 				'featured_image' => $featuredUrl,
 			];
+
+			// Metadata
+			$meta = [];
+			try {
+				$crawler->filter( 'head > meta' )->each( function ( $node ) use ( &$meta ) {
+					$property = $node->attr( 'property' );
+					if ( $property === 'og:title' ) {
+						$meta['title'] = $node->attr( 'content' );
+					}
+
+					if ( $property === 'og:description' ) {
+						$meta['description'] = $node->attr( 'content' );
+					}
+				} );
+			} catch ( Exception $e ) {
+				WP_CLI::warning( 'No Metadata found ', $e->getMessage() );
+			}
+
+			return [
+				'post' => $post,
+				'meta' => $meta,
+			];
+
 		} catch ( Exception $e ) {
 			WP_CLI::warning( 'Post not created : ' . $post );
 
